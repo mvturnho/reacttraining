e7d58979777d7b00d99a35686634a789a4603361
mvturnho <qris.online@gmail.com>
2023-04-20 20:37:07 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, FormGroup, FormControl } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FormControl className="m-4 fullwidth" type="file" onChange={handleFile} />
     </div>
   );
+}
+
+function handleFile(event) {
+  const newFile = event.target.files[0];
+  if (!newFile) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    // var content = e.target.result;
+    console.log(e.target.result);
+  }
+
+  reader.readAsText(newFile, 'utf-8');
 }
 
 export default App;
